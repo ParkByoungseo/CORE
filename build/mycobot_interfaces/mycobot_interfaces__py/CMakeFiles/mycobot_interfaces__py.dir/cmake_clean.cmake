f9de60c904f6fca69432abfc132cb158a3ba1242
Your Name <your.email@example.com>
2024-11-26 15:52:52 +0900

    [FEAT] ros2_gazebo_controller_excavator: initial setup

---
+++ build/mycobot_interfaces/mycobot_interfaces__py/CMakeFiles/mycobot_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../rosidl_generator_py/mycobot_interfaces/_mycobot_interfaces_s.ep.rosidl_typesupport_c.c"
+  "../rosidl_generator_py/mycobot_interfaces/_mycobot_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "../rosidl_generator_py/mycobot_interfaces/_mycobot_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "../rosidl_generator_py/mycobot_interfaces/srv/__init__.py"
+  "../rosidl_generator_py/mycobot_interfaces/srv/_get_planning_scene.py"
+  "../rosidl_generator_py/mycobot_interfaces/srv/_get_planning_scene_s.c"
+  "CMakeFiles/mycobot_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mycobot_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ install/mycobot_interfaces/local/lib/python3.10/dist-packages/mycobot_interfaces/srv/__init__.py
@@ -0,0 +1,1 @@
+from mycobot_interfaces.srv._get_planning_scene import GetPlanningScene  # noqa: F401
